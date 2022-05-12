4050a8be881a9275fbefd4b9c3721f2eb4021e33
aozeyu <https://github.com/aozeyu>
2022-05-12 13:09:51 +0800

    Initial commit

---
+++ xdclass-common/src/main/java/net/xdclass/domain/User.java
@@ -0,0 +1,11 @@
+package net.xdclass.domain;
+
+/**
+ * @program: xdclass-cloud
+ * @description: 22
+ * @packagename: net.xdclass.domain
+ * @author: 姚泽宇
+ * @date: 2022-05-12 13:09
+ **/
+public class User {
+}
